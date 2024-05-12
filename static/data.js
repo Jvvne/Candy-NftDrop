adbecaef2626052c2ef7ce94445e33a2e2be811d
Jorge Miguel Garcia <148028363+Jvvne@users.noreply.github.com>
2024-05-11 23:45:20 -0700

    Add files via upload

---
+++ static/data.js
@@ -0,0 +1,34 @@
+export const data = {
+    "program": {
+        "candyMachine": "9rY7xSHnmfvenkwGmweFFnEmADQXTULGDwgcczYMbjR7",
+        "candyGuard": "",
+        "candyMachineCreator": "CdmBLjRGi7RJSLDWRAmfcXEswwrXbTUry274Ko2n7F43",
+        "collectionMint": "HZUu9VLcbUW8z3RwHgzcFYxiSpNgor6ahZLGM9MRy6Hi"
+    },
+    "items": [
+        {
+            "name": "Pudgy Penguin #3950",
+            "image_hash": "\u000f%�V�$���\u001d�4��\u0004��J2\u001d�>\u001f�n���QC'Y",
+            "image_link": "https://arweave.net/2ZP1RZS-QLh_3dpU34KzURQ_C_jH9dU8K2SZ3Kez8Cc",
+            "metadata_hash": "�7eRSt2b�\u0001[�G�卪����\u0002�<�\u0019c\u0017kU3B",
+            "metadata_link": "https://arweave.net/NK7Vvv40Mu3uuzMpeytyWisNwig27VhEJnMBboKUojA",
+            "onChain": true
+          },
+          {
+            "name": "Pudgy Penguin #7365",
+            "image_hash": "3v\u001a&]��3��\u0001�)��X�c%_\u001fTB�)U\u0011�YY",
+            "image_link": "https://arweave.net/cHogGwH-18HjvzZ0WOrmxPsVIeMH47l-DqX7t1FFqGc",
+            "metadata_hash": "�.d����9$H\\��\u000e3�@����\u0002#�\u001a�M�� ��",
+            "metadata_link": "https://arweave.net/QkDWtKG_teo3MUMuYaqSkKCHNwfkvydfKsE-1pm78_w",
+            "onChain": true
+          },
+          {
+            "name": "Pudgy Penguin #5678",
+            "image_hash": "�a���\u0019�;\u0016oA-]\b%ꁒ7��4ⷽ�:��F",
+            "image_link": "https://arweave.net/RjkBYfjsBJxJv0ejNmQvfXSWfXCD4aexvRT9LN-FaHI",
+            "metadata_hash": "�\u001aT�\u001eY�\u0018T����(w��\u001c\u0011���L�� �R\u0001\u0016��",
+            "metadata_link": "https://arweave.net/03NoV-uwnZBHUxtAdjzSVrbU6UmwD8ziyY0ATmDJtUQ",
+            "onChain": true
+          },
+    ]
+}
