280335b9042bfbb4a50e4b3ced8820f70ec68175
Jorge Miguel Garcia <148028363+Jvvne@users.noreply.github.com>
2024-05-12 23:01:14 -0700

    .k..

---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://skilled-solitary-panorama.solana-devnet.quiknode.pro/eadb3f2aa3cdd7315db1a8230530981f183b39f3/";
+
+// Change this with your candy machine id
+export const CANDY_MACHINE_ID = new PublicKey("AzCBTmnVfUzRuxDS6wuxoxvcJBfFwMdbv4hcHVb6vPNh");
